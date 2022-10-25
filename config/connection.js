faf7ed821710abfe8e57fd06effbe578e91f0c78
ImDiplan <81445700+ImDiplan@users.noreply.github.com>
2022-10-25 01:09:04 -0300

    making connection config

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,3 @@
 let mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://root:root@cluster0.8ay6i.mongodb.net/crud')
+mongoose.connect('mongodb+srv://root:root@cluster0.8ay6i.mongodb.net/crud');
 module.exports = mongoose;
